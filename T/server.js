5d870033e87ad515175f882a93f04221a3540b49
Br0d1e <aurecords1@outlook.com>
2026-05-07 11:32:36 +0800

    Update server.js

--- T/server.js
+++ T/server.js
@@ -1,140 +1,52 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const app = express();
-
 app.use(cors());
 
+const PORT = process.env.PORT || 3000;
+
+/* ALLOWED K-POP KEYWORDS */
 const allowedKeywords = [
-
   // GENERAL
-  "kpop",
-  "k-pop",
-  "album",
-  "photocard",
-  "lightstick",
-  "merch",
-  "season greetings",
-  "signed",
-  "pob",
-  "preorder",
-  "digipack",
+  "kpop", "k-pop", "album", "photocard", "lightstick", "merch",
+  "season greetings", "signed", "pob", "preorder", "digipack",
 
-  // BIG BOY GROUPS
-  "bts",
-  "seventeen",
-  "stray kids",
-  "txt",
-  "tomorrow x together",
-  "enhypen",
-  "ateez",
-  "exo",
-  "nct",
-  "nct 127",
-  "nct dream",
-  "wayv",
-  "riize",
-  "treasure",
-  "the boyz",
-  "zerobaseone",
-  "zb1",
-  "boynextdoor",
-  "got7",
-  "monsta x",
-  "shinee",
-  "super junior",
-  "bigbang",
-  "ikon",
-  "winner",
-  "2pm",
-  "day6",
-  "xdinary heroes",
-  "btob",
-  "infinite",
-  "astro",
-  "oneus",
-  "p1harmony",
-  "evnne",
-  "tempest",
-  "all(h)ours",
+  // GROUPS
+  "bts", "seventeen", "stray kids", "txt", "tomorrow x together",
+  "enhypen", "ateez", "exo", "nct", "nct 127", "nct dream", "wayv",
+  "riize", "treasure", "the boyz", "zerobaseone", "zb1",
+  "boynextdoor", "got7", "monsta x", "shinee", "super junior",
+  "bigbang", "ikon", "winner", "2pm", "day6", "xdinary heroes",
+  "btob", "infinite", "astro", "oneus", "p1harmony", "evnne",
+  "tempest", "all(h)ours",
 
-  // BIG GIRL GROUPS
-  "blackpink",
-  "twice",
-  "newjeans",
-  "aespa",
-  "ive",
-  "le sserafim",
-  "lesserafim",
-  "twice",
-  "itzy",
-  "nmixx",
-  "gidle",
-  "(g)i-dle",
-  "idle",
-  "babymonster",
-  "kiss of life",
-  "kiof",
-  "illit",
-  "izna",
-  "katseye",
-  "hearts2hearts",
-  "meovv",
-  "fifty fifty",
-  "kep1er",
-  "stayc",
-  "fromis_9",
-  "red velvet",
-  "mamamoo",
-  "oh my girl",
-  "dreamcatcher",
-  "weeekly",
-  "viviz",
-  "wooah",
-  "csr",
-  "triples",
-  "tripleS",
-  "class:y",
-  "young posse",
-  "badvillain",
-  "rescene",
-  "unis",
-  "h1-key",
-  "cignature",
-  "purple kiss",
-
-  // CO-ED / OTHER
-  "kard",
-  "xlov",
+  // GIRL GROUPS
+  "blackpink", "twice", "newjeans", "aespa", "ive",
+  "le sserafim", "lesserafim", "itzy", "nmixx", "gidle",
+  "(g)i-dle", "idle", "babymonster", "kiss of life", "kiof",
+  "illit", "izna", "katseye", "hearts2hearts", "meovv",
+  "fifty fifty", "kep1er", "stayc", "fromis_9", "red velvet",
+  "mamamoo", "oh my girl", "dreamcatcher", "weeekly", "viviz",
+  "wooah", "csr", "triples", "tripleS", "class:y",
+  "young posse", "badvillain", "rescene", "unis",
+  "h1-key", "cignature", "purple kiss",
 
   // SOLOISTS
-  "iu",
-  "taeyeon",
-  "jungkook",
-  "jimin",
-  "v",
-  "rm",
-  "suga",
-  "j-hope",
-  "jhope",
-  "rose",
-  "rosé",
-  "jennie",
-  "lisa",
-  "jisoo",
-  "cha eunwoo",
-  "sunmi",
-  "chungha",
-  "somi"
+  "iu", "taeyeon", "jungkook", "jimin", "v", "rm", "suga",
+  "j-hope", "jhope", "rose", "rosé", "jennie", "lisa",
+  "jisoo", "cha eunwoo", "sunmi", "chungha", "somi"
 ];
 
+/* SEARCH ROUTE */
 app.get("/search", async (req, res) => {
   try {
     const query = (req.query.q || "").toLowerCase().trim();
 
-    /* BLOCK NON K-POP SEARCHES */
+    // BLOCK NON K-POP SEARCHES
     const isKpopSearch = allowedKeywords.some(keyword =>
       query.includes(keyword)
     );
@@ -146,10 +58,9 @@ app.get("/search", async (req, res) => {
     }
 
     const apiURL =
-  `https://serpapi.com/search.json?engine=google_shopping&q=${encodeURIComponent(query)}&api_key=${process.env.SERP_API_KEY}`;
+      `https://serpapi.com/search.json?engine=google_shopping&q=${encodeURIComponent(query)}&api_key=${process.env.SERP_API_KEY}`;
 
     const response = await fetch(apiURL);
-
     const data = await response.json();
 
     const results = (data.shopping_results || []).map(item => ({
@@ -160,19 +71,17 @@ app.get("/search", async (req, res) => {
       thumbnail: item.thumbnail
     }));
 
-    console.log(results[0]);
-
     res.json(results);
 
   } catch (err) {
     console.error(err);
-
     res.status(500).json({
       error: "Something went wrong"
     });
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+/* START SERVER (ONLY ONCE) */
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
