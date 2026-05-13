d4909e943d3c20c912968e3ce42d90696890c255
Br0d1e <aurecords1@outlook.com>
2026-05-13 10:07:43 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ dotenv.config();
 const app = express();
 app.use(cors());
 
+app.get("/search", (req, res) => {
+  const query = req.query.q;
+
+  if (!query) {
+    return res.send("No search query provided");
+  }
+
+  res.send(`You searched for: ${query}`);
+});
+
 const PORT = process.env.PORT || 3000;
 
 /* SEARCH ROUTE */
